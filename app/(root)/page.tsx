0831bf7ad4ae5d20867ff02757a0eda7db715cf5
dale <stewartdale45@yahoo.com>
2024-05-14 18:00:28 -0400

    Completed spending and Pagination and Spending Categories

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -19,6 +19,8 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
 
     const account = await getAccount({ appwriteItemId })
 
+    // console.log({accountsData,account})
+
     return (
         <section className="home">
             <div className="home-content">
