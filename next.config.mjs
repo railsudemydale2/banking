b8a8006dce275b19bb056cccfd8c03e90ebd0204
dale <stewartdale45@yahoo.com>
2024-05-14 20:36:50 -0400

    modified next.config.mjs file to reflect correct organization

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ export default withSentryConfig(withSentryConfig(withSentryConfig(nextConfig, {
 
 // Suppresses source map uploading logs during build
 silent: true,
-org: "jsm-x9",
+org: "jsm-td",
 project: "javascript-nextjs",
 }, {
 // For all available options, see:
